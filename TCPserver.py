68450fc2daa2ee36e674fb38ad347bb05bddb297
iangalvao <iansgalvao@gmail.com>
2022-05-19 16:10:29 -0300

    threading init

--- TCPserver.py
+++ TCPserver.py
@@ -1,12 +1,11 @@
 import socket
+from threading import Thread
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
+
+def handleClient(conn, addr):
     with conn:
         print(f"Connected by {addr}")
         while True:
@@ -14,3 +13,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             if not data:
                 break
             conn.sendall(data)
+
+
+threads = []
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    while True:
+        conn, addr = s.accept()
+        t = Thread(target=handleClient, args=(conn, addr,))
+        threads.append(t)
+        t.start()
