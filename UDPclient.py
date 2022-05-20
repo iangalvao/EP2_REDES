98849cff9c044a9ac1a67de27504457e1cdc917a
iangalvao <iansgalvao@gmail.com>
2022-05-19 23:47:04 -0300

    merged TCP/UDP

---
+++ UDPclient.py
@@ -0,0 +1,18 @@
+import socket
+
+PORT = 65432
+msgFromClient = "Hello UDP Server"
+bytesToSend = str.encode(msgFromClient)
+
+serverAddressPort = ("127.0.0.1", PORT)
+bufferSize = 1024
+
+# Create a UDP socket at client side
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Send to server using created UDP socket
+UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+msg = "Message from Server {}".format(msgFromServer[0])
+print(msg)
--- server.py
+++ server.py
@@ -1,29 +1,62 @@
+
 import socket
+from threading import Thread
 
-localIP = "127.0.0.1"
-localPort = 20001
-bufferSize = 1024
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+BUFFERSIZE = 1024
 
-msgFromServer = "Hello UDP Client"
-bytesToSend = str.encode(msgFromServer)
 
-# Create a datagram socket
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+def handleUDPClient(bytesAddressPair, s):
+    msgFromServer = "Hello UDP Client"
+    bytesToSend = str.encode(msgFromServer)
 
-# Bind to address and ip
-UDPServerSocket.bind((localIP, localPort))
-
-print("UDP server up and listening")
-
-# Listen for incoming datagrams
-while(True):
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
     message = bytesAddressPair[0]
     address = bytesAddressPair[1]
     clientMsg = "Message from Client:{}".format(message)
     clientIP = "Client IP Address:{}".format(address)
     print(clientMsg)
     print(clientIP)
+    s.sendto(bytesToSend, address)
 
-    # Sending a reply to client
-    UDPServerSocket.sendto(bytesToSend, address)
+
+def listenUDP(host, port, bufferSize):
+    threadsUDP = []
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.bind((HOST, port))
+        print("bind")
+        while(True):
+            print("listening UDP")
+            bytesAddressPair = s.recvfrom(bufferSize)
+            print("Received UDP message")
+            t = Thread(target=handleUDPClient, args=(bytesAddressPair, s,))
+            threadsUDP.append(t)
+            t.start()
+
+
+def listenTCP(host, port):
+    threadsTCP = []
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        while True:
+            conn, addr = s.accept()
+            t = Thread(target=handleTCPClient, args=(conn, addr,))
+            threadsTCP.append(t)
+            t.start()
+
+
+def handleTCPClient(conn, addr):
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+
+
+tcpT = Thread(target=listenTCP, args=(HOST, PORT,))
+tcpT.start()
+udpT = Thread(target=listenUDP, args=(HOST, PORT, BUFFERSIZE,))
+udpT.start()
