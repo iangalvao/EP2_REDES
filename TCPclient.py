04fe28ede3d60d00ea61bc3a03259772010e2550
iangalvao <iansgalvao@gmail.com>
2022-05-22 02:13:25 -0300

    conn in

--- TCPclient.py
+++ TCPclient.py
@@ -1,13 +1,56 @@
 # echo-client.py
 
+import sys
 import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+
+def sendHello(host, port):
+
     s.sendall(b"Hello, world")
     data = s.recv(1024)
+    print(f"Received {data!r}")
 
-print(f"Received {data!r}")
+
+def packNew(user, password):
+    command = "newU"
+    usernameLen = len(user)
+    message = f"{command}{usernameLen:03d}{user}{password}"
+    return message
+
+
+def packIn(user, password):
+    command = "in__"
+    usernameLen = len(user)
+    message = f"{command}{usernameLen:03d}{user}{password}"
+    return message
+
+
+if len(sys.argv) == 3:
+    port = (int)(sys.argv[1])
+    print(port)
+    host = sys.argv[2]
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((host, port))
+    while True:
+        comm = input("JogoDaVelha>").split()
+        if comm:
+            if comm[0] == "bye":
+                break
+            elif comm[0] == "hello":
+                sendHello(HOST, PORT)
+            elif comm[0] == "new":
+                if len(comm) != 3:
+                    print("uso: new <usuario> <senha>")
+                else:
+                    message = packNew(comm[1], comm[2])
+                    s.sendall(bytes(message, encoding="ASCII"))
+            elif comm[0] == "in":
+                if len(comm) != 3:
+                    print("uso: in <usuario> <senha>")
+                else:
+                    message = packIn(comm[1], comm[2])
+                    s.sendall(bytes(message, encoding="ASCII"))
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-
+import sys
 import socket
 from threading import Thread
 
@@ -7,16 +7,49 @@ PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 BUFFERSIZE = 1024
 
 
+def handleNew(data, users, connectedUsers):
+    unameLen = int(data[4:7])
+    username = data[7:7+unameLen]
+    password = data[7+unameLen:]
+    print(
+        f"username length:{unameLen}\nusername:{username}\npassword:{password}")
+    users[username] = password
+    print(users)
+
+
+def handleLogin(data, users, connectedUsers):
+    unameLen = int(data[4:7])
+    username = data[7:7+unameLen]
+    password = data[7+unameLen:]
+    print(
+        f"username length:{unameLen}\nusername:{username}\npassword:{password}")
+    if users[username] == password:
+        print(f"user {username}: connected.")
+        connectedUsers[username] = 1
+    else:
+        pass
+
+
+def handleList(data, connectedUsers):
+    message = ""
+    for user, status in connectedUsers.keys():
+        message += f"{len(user):03d}"
+        message += user
+        message += statusToStr(status)
+        pass
+
+
+def handleHoF(data, users):
+    pass
+
+
 def handleUDPClient(bytesAddressPair, s):
     msgFromServer = "Hello UDP Client"
     bytesToSend = str.encode(msgFromServer)
 
     message = bytesAddressPair[0]
     address = bytesAddressPair[1]
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP = "Client IP Address:{}".format(address)
-    print(clientMsg)
-    print(clientIP)
+    print(f"Connected by {address} using UDP.")
     s.sendto(bytesToSend, address)
 
 
@@ -24,39 +57,58 @@ def listenUDP(host, port, bufferSize):
     threadsUDP = []
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.bind((HOST, port))
-        print("bind")
         while(True):
-            print("listening UDP")
             bytesAddressPair = s.recvfrom(bufferSize)
-            print("Received UDP message")
             t = Thread(target=handleUDPClient, args=(bytesAddressPair, s,))
             threadsUDP.append(t)
             t.start()
 
 
-def listenTCP(host, port):
+def listenTCP(host, port, users):
     threadsTCP = []
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
+        s.bind((HOST, port))
         s.listen()
         while True:
             conn, addr = s.accept()
-            t = Thread(target=handleTCPClient, args=(conn, addr,))
+            t = Thread(target=handleTCPClient, args=(conn, addr, users, ))
             threadsTCP.append(t)
             t.start()
 
 
-def handleTCPClient(conn, addr):
+users = {}
+connectedUsers = {}
+
+
+def handleTCPClient(conn, addr, users):
     with conn:
-        print(f"Connected by {addr}")
+        print(f"Connected by {addr} using TCP")
         while True:
             data = conn.recv(1024)
+            print(data)
             if not data:
                 break
-            conn.sendall(data)
+            comm = data[0:4]
+            print(comm)
+            if comm == b"newU":
+                handleNew(data, users, connectedUsers)
+
+            elif comm == b"in__":
+                handleLogin(data, users, connectedUsers)
+
+            elif comm == b"list":
+                handleList(data, connectedUsers)
+
+            elif comm == b"hall":
+                handleHoF(data, users)
+
+    # conn.sendall(data)
 
 
-tcpT = Thread(target=listenTCP, args=(HOST, PORT,))
+if len(sys.argv) == 2:
+    port = (int)(sys.argv[1])
+    print(port)
+tcpT = Thread(target=listenTCP, args=(HOST, port, users,))
 tcpT.start()
-udpT = Thread(target=listenUDP, args=(HOST, PORT, BUFFERSIZE,))
+udpT = Thread(target=listenUDP, args=(HOST, port, BUFFERSIZE,))
 udpT.start()
