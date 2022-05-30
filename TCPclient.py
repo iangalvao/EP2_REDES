728c297d8e35e0cf0c0df2db2aaadd19aceb3fa1
iangalvao <iansgalvao@gmail.com>
2022-05-29 22:41:36 -0300

    pass change

--- TCPclient.py
+++ TCPclient.py
@@ -21,11 +21,26 @@ def packNew(user, password):
     return message
 
 
+def packPass(old, new):
+    command = "pass"
+    oldLen = len(old)
+    message = f"{command}{oldLen:03d}{old}{new}"
+    return message
+
+
 def packIn(user, password):
     command = "in__"
     usernameLen = len(user)
     message = f"{command}{usernameLen:03d}{user}{password}"
     return message
+
+
+def packList():
+    return "list"
+
+
+def packOut():
+    return "out_"
 
 
 if len(sys.argv) == 3:
@@ -53,4 +68,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     print("uso: in <usuario> <senha>")
                 else:
                     message = packIn(comm[1], comm[2])
+                    s.sendall(bytes(message, encoding="ASCII"))
+            elif comm[0] == "list":
+                message = packList()
+                message = str.encode(message)
+                s.sendall(message)
+                message = s.recv(1024)
+                print(message.decode('utf-8'))
+            elif comm[0] == "out":
+                message = packOut()
+                s.sendall(bytes(message, encoding="ASCII"))
+            elif comm[0] == "pass":
+                if len(comm) != 3:
+                    print("uso: pass <senha> <nova_senha>")
+                else:
+                    message = packPass(comm[1], comm[2])
                     s.sendall(bytes(message, encoding="ASCII"))
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import sys
 import socket
-from threading import Thread
+from threading import Thread, get_ident
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
@@ -9,38 +9,74 @@ BUFFERSIZE = 1024
 
 def handleNew(data, users, connectedUsers):
     unameLen = int(data[4:7])
-    username = data[7:7+unameLen]
-    password = data[7+unameLen:]
-    print(
-        f"username length:{unameLen}\nusername:{username}\npassword:{password}")
+    username = data[7:7+unameLen].decode('utf-8')
+    password = data[7+unameLen:].decode('utf-8')
     users[username] = password
-    print(users)
+    # send(newACK)
+
+
+def handlePass(data, users, connectedUsers):
+    oldLen = int(data[4:7])
+    oldPass = data[7:7+oldLen].decode('utf-8')
+    newPass = data[7+oldLen:].decode('utf-8')
+    username = connectedUsers[get_ident()]
+    if oldPass == users[username]:
+        users[username] = newPass
+    else:
+        print(
+            f"Password Change Attempt Failed. User:{username}, password:{oldPass}")
+    # send(newACK)
 
 
 def handleLogin(data, users, connectedUsers):
     unameLen = int(data[4:7])
-    username = data[7:7+unameLen]
-    password = data[7+unameLen:]
+    username = data[7:7+unameLen].decode('utf-8')
+    password = data[7+unameLen:].decode('utf-8')
     print(
-        f"username length:{unameLen}\nusername:{username}\npassword:{password}")
+        f"LOGIN username length:{unameLen}\nusername:{username}\npassword:{password}")
     if users[username] == password:
         print(f"user {username}: connected.")
-        connectedUsers[username] = 1
+        connectedUsers[get_ident()] = username
+        # send(loginACK)
     else:
+        # send(loginNACK)
+        print(f"Login Attempt Failed. User = {username}, pass: {password}")
         pass
 
 
-def handleList(data, connectedUsers):
+def handleLogout(connectedUsers, s):
+    connectedUsers.pop(get_ident())
+    # send(logoutACK)
+    print(connectedUsers)
+
+
+def handleList(data, connectedUsers, s):
+    message = createList(data, connectedUsers)
+    message = str.encode(message)
+    if not message:
+        message = b"There are no users connected."
+    s.sendall(message)
+
+
+def handleHoF(data, users, s):
+    pass
+
+
+def statusToStr(status):
+    if status != 0:
+        return "Connected"
+    else:
+        return "Disconnected"
+
+
+def createList(data, connectedUsers):
     message = ""
-    for user, status in connectedUsers.keys():
+    for tnum, user in connectedUsers.items():
         message += f"{len(user):03d}"
         message += user
-        message += statusToStr(status)
-        pass
-
-
-def handleHoF(data, users):
-    pass
+        message += statusToStr(tnum)
+        message += '\n'
+    return message
 
 
 def handleUDPClient(bytesAddressPair, s):
@@ -51,6 +87,37 @@ def handleUDPClient(bytesAddressPair, s):
     address = bytesAddressPair[1]
     print(f"Connected by {address} using UDP.")
     s.sendto(bytesToSend, address)
+
+
+def handleTCPClient(conn, addr, users):
+    with conn:
+        print(f"Connected by {addr} using TCP")
+        while True:
+            data = conn.recv(1024)
+            print(data)
+            if not data:
+                break
+            comm = data[0:4]
+            print(comm)
+            if comm == b"newU":
+                handleNew(data, users, connectedUsers)
+
+            elif comm == b"in__":
+                handleLogin(data, users, connectedUsers)
+
+            elif comm == b"list":
+                handleList(data, connectedUsers, conn)
+
+            elif comm == b"hall":
+                handleHoF(data, users, conn)
+            elif comm == b"out_":
+                print("starting logout")
+                handleLogout(connectedUsers, conn)
+            elif comm == b"pass":
+                handlePass(data, users, connectedUsers)
+            else:
+                print("not a command")
+    # conn.sendall(data)
 
 
 def listenUDP(host, port, bufferSize):
@@ -80,34 +147,11 @@ users = {}
 connectedUsers = {}
 
 
-def handleTCPClient(conn, addr, users):
-    with conn:
-        print(f"Connected by {addr} using TCP")
-        while True:
-            data = conn.recv(1024)
-            print(data)
-            if not data:
-                break
-            comm = data[0:4]
-            print(comm)
-            if comm == b"newU":
-                handleNew(data, users, connectedUsers)
-
-            elif comm == b"in__":
-                handleLogin(data, users, connectedUsers)
-
-            elif comm == b"list":
-                handleList(data, connectedUsers)
-
-            elif comm == b"hall":
-                handleHoF(data, users)
-
-    # conn.sendall(data)
-
-
 if len(sys.argv) == 2:
     port = (int)(sys.argv[1])
     print(port)
+else:
+    port = PORT
 tcpT = Thread(target=listenTCP, args=(HOST, port, users,))
 tcpT.start()
 udpT = Thread(target=listenUDP, args=(HOST, port, BUFFERSIZE,))
